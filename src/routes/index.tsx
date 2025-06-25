db1ec28863e613507d6b15bc5be2ff0f427dd5b6
HoangMinh456 <xinphepgiauten456@gmail.com>
2025-06-25 16:13:05 +0700

    update Detail Page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from "react-router"
 import LayoutWebsite from "../pages/(website)/layout"
 import HomePage from "../pages/(website)/homepage/page"
+import ProductDetail from "@/pages/(website)/productdetail/page"
+// import TestPage from "@/pages/(website)/test/page"
 
 const Router = () => {
     return (
         <Routes>
             <Route path="/" element={<LayoutWebsite />}>
                 <Route index element={<HomePage />} />
+                <Route path="/detail" element={<ProductDetail />} />
             </Route>
         </Routes>
     )
